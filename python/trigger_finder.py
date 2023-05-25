84b42b8a2bc5bd12eae6d25e4deda99f1743ea65
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2023-05-25 20:09:18 +0200

    Remove SimpleTriggerFinder, make RobustTriggerFinder a dataclass

--- python/trigger_finder.py
+++ python/trigger_finder.py
@@ -1,103 +1,26 @@
-# TODO find trigger stat time is so long because the callback is part of it, and is measured with it
-#      --> measure trigger finding by itself
-
-# TODO perf --> disp2depth is the slow one: particularly the 255 masking in the color map :o
-
-# TODO perf: next biggest perf issue: the trigger concat --> use a ring buffer
-
 import numpy as np
-import time
 from metavision_sdk_base import EventCD
-
-
-class SimpleTriggerFinder:
-    def __init__(self, projector_fps=60, frame_paused_thresh_us=40, stats=None, callback=None):
-        # when there is a time diff between consecutive events of more than this, we assume
-        # that the projector is currently not projecting a frame
-        self.frame_paused_thresh_us = frame_paused_thresh_us
-
-        self.projector_fps = projector_fps
-
-        self.ev_buf = np.array([], dtype=EventCD)
-
-        self.stats = stats
-
-        self.callback = callback
-
-    def process_events(self, evs):
-        # TODO perf use a ring buffer
-        # e.g. np-rw-buffer
-        # TODO investigate: EventIterator from Metavision just uses a deque in MetaEventBufferProducer
-        self.ev_buf = np.append(self.ev_buf, evs.numpy())
-
-        if len(self.ev_buf) < 2:
-            return
-
-        # if we have fewer than one frame worth of events, don't bother
-        if self.ev_buf["t"][-1] - self.ev_buf["t"][0] < 1e6 / self.projector_fps:
-            return
-
-        self.stats.add_metric("#events in buf [k]", len(self.ev_buf) / 1000)
-        self.stats.add_metric("event t range in buf [ms]", (self.ev_buf["t"][-1] - self.ev_buf["t"][0]) / 1000)
-
-        with self.stats.measure_time("find trigger"):
-            found = self.find_trigger()
-
-        if found:
-            self.stats.count_occurrence("trigger found")
-        else:
-            self.stats.count_occurrence("no trigger found")
-
-    def find_trigger(self):
-        """function to get the frame starts and ends from the current buffer."""
-
-        # find the indicies of the events which have a relative long time difference to their respective next event in the buffer
-
-        # TODO perf: this is recomputed a lot
-        frame_paused_ev_idx = np.nonzero(np.diff(self.ev_buf["t"]) >= self.frame_paused_thresh_us)[0]
-
-        for prev_idx, next_idx in zip(frame_paused_ev_idx[:-1], frame_paused_ev_idx[1:]):
-            # time between event pauses
-            diff_t_event_pauses = self.ev_buf["t"][next_idx] - self.ev_buf["t"][prev_idx]
-
-            # if this time is greater than half of the frametime, it
-            # is assumend that the two events are the first and last events of the frame
-
-            if diff_t_event_pauses > 1e6 / self.projector_fps / 2:
-                # TODO reexamine the +2 and -2
-                # The interval is trimmed a a little to ensure that the events are in the frame.
-                self.callback(self.ev_buf[prev_idx + 2 : next_idx - 2])
-
-                self.stats.add_metric(
-                    "frame len [ms]", (self.ev_buf["t"][next_idx - 2] - self.ev_buf["t"][prev_idx + 2]) / 1000
-                )
-
-                self.ev_buf = self.ev_buf[next_idx - 2 :]
-                return True
-
-        return False
+from dataclasses import dataclass
+from typing import Callable
+from stats_printer import StatsPrinter
 
 
 MIN_EVENTS_PER_FRAME = 1000
 
 
+@dataclass
 class RobustTriggerFinder:
-    def __init__(self, projector_fps=60, frame_paused_thresh_us=40, stats=None, callback=None):
-        # when there is a time diff between consecutive events of more than this, we assume
-        # that the projector is currently not projecting a frame
-        self.frame_paused_thresh_us = frame_paused_thresh_us
+    projector_fps: int
+    stats: StatsPrinter
+    callback: Callable[[np.ndarray], None]
 
-        self.projector_fps = projector_fps
+    frame_paused_thresh_us = 40
+    should_drop = False
+    ev_buf = np.array([], dtype=EventCD)
 
-        self.ev_buf = np.array([], dtype=EventCD)
-
-        self.stats = stats
-
-        self.callback = callback
-
-        self.frame_len_ms = 1e3 / self.projector_fps
-
-        self.should_drop = False
+    @property
+    def frame_len_ms(self):
+        return 1e3 / self.projector_fps
 
     def reset_buffer(self):
         self.ev_buf = np.array([], dtype=EventCD)
